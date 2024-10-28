7fefef2818ae178446c44c42b9d110575383586a
BarrigaAmarela <137063434+BarrigaAmarela@users.noreply.github.com>
2024-10-28 11:49:14 -0300

    aaaaaaa

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criarCartao(
+    'História',
+    'Qual é o estado mais novo do Brasil?',
+    'Tocantins'
+)
+
+criarCartao(
+    'Matemática',
+    'O que é o "X"?',
+    'O "X" pode ser qualquer número que desconhecemos o valor'
+)
+
+criarCartao(
+    'Geografia',
+    'O que é uma Fronteira?',
+    'È uma linha que marca o fim de um País e/ou e o inicio de outro'
+)
+
+criarCartao(
+    'ingles',
+    '"Comida" em ingles?',
+    'Food'
+)
+
+criarCartao(
+    'Portugues',
+    'Aumentativo de mesa?',
+    'Mesona / Mesão'
+)
+
+criarCartao(
+    'Matemática',
+    'Quanto é 9x9?',
+    '81'
+)
+
+criarCartao(
+    'História',
+    'Quem Descobriu o Brasil?',
+    'Pedro de Alcântara Francisco Antônio João Carlos Xavier de Paula Miguel Rafael Joaquim José Gonzaga Pascoal Cipriano Serafim de Bragança e Bourbon'
+)
+
+criarCartao(
+    'Geografia',
+    'O que é um Relevo?',
+    'O relevo é definido como sendo as formas da superfície de um planeta'
+)
+
+criarCartao(
+    'Portugues',
+    'Que lingua falam em Cuba?',
+    'Espanhol'
+)
+
